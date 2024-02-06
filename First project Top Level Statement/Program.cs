b59f4804c672783d4a82ed62b037f1583f182fe0
Nadir Zamanov <Zamanov@itstep.edu.az>
2024-02-06 17:54:48 +0400

    Add project files.

---
+++ First project Top Level Statement/Program.cs
@@ -0,0 +1,35 @@
+﻿//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, Step!");
+
+// Value type - значимые типы 
+// - наследуется от struct
+// - создается в stack
+
+bool ch = true;             // Boolean
+
+byte byt = 15;              // Byte
+sbyte sbyt = 0;             // SByte
+char symbol = 'a';          // Char
+short srt = default;        // Int16
+ushort usrt = 25;           // UInt16
+int number = -3265;         // Int32
+uint number1 = 3265;        // UInt32
+long number2 = 264618;      // Int64
+ulong number3 = 264618;     // UInt64
+
+float flt = 0.65F;          // Single
+double dbl = .3659;         // Double
+
+decimal dec = 365.236M;     // Decimal
+
+
+// Reference type - ссылочный тип
+// - наследуется от class
+// - создается в heap
+// - создается через new
+
+string name = "Nadir";                      // String
+string name2 = new string("Nadir");         // String
+
+//Console.WriteLine(sizeof(int));
+//Console.WriteLine(typeof(string));
---
+++ First project/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace First_project;
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
