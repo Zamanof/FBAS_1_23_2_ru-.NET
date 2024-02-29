bc4a196656333227b26a3ff395100445e7a1fde1
Nadir Zamanov <Zamanov@itstep.edu.az>
2024-02-29 16:02:32 +0400

    Func, Action, Predicate, Comparison 2

--- C# 08. Delegates/Program.cs
+++ C# 08. Delegates/Program.cs
@@ -128,46 +128,136 @@
 #endregion
 
 // Action<T>, Func<T>, Predicate<T>, Comparison<T>
+using System.Threading.Channels;
 
+List<int> numbers = new List<int>() { 1, 119, 5, 865, 45, 66, 111, 22, 33, 44 };
 #region Action
-int[] arr = new int[25];
-Random random = new Random();
-for (int i = 0; i < arr.Length; i++)
-{
-    arr[i] = random.Next(10, 99);
-}
-foreach (int i in arr)
-{
-    Console.Write($"{i} ");
-}
-Console.WriteLine();
+//int[] arr = new int[25];
+//Random random = new Random();
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    arr[i] = random.Next(10, 99);
+//}
+//foreach (int i in arr)
+//{
+//    Console.Write($"{i} ");
+//}
+//Console.WriteLine();
 
-//Opertions(arr, ShowSquare);
-Opertions(arr, ShowNegative);
-Action<int, double> action1 = Foo1;
-action1.Invoke(2, 63.6);
-void Opertions(int[] ints, Action<int> action)
-{
-    foreach(var item in ints)
-    {
-        action(item);
-    }
-    Console.WriteLine();
-}
+////Opertions(arr, ShowSquare);
+//Opertions(arr, ShowNegative);
+//Action<int, double> action1 = Foo1;
+//action1.Invoke(2, 63.6);
+//void Opertions(int[] ints, Action<int> action)
+//{
+//    foreach (var item in ints)
+//    {
+//        action(item);
+//    }
+//    Console.WriteLine();
+//}
 
-void ShowSquare(int num)
-{
-    Console.Write($"{num * num} ");
-}
+//void ShowSquare(int num)
+//{
+//    Console.Write($"{num * num} ");
+//}
 
-void ShowNegative(int num)
-{
-    Console.Write($"{-num} ");
-}
+//void ShowNegative(int num)
+//{
+//    Console.Write($"{-num} ");
+//}
 
-void Foo1(int a, double b)
-{
-    Console.WriteLine(a*b);
-}
+//void Foo1(int a, double b)
+//{
+//    Console.WriteLine(a * b);
+//}
 
+#endregion
+
+#region Func
+// Func<T>
+
+//var evens = filterNumbers(numbers, isEven);
+//var odds = filterNumbers(numbers, isOdd);
+////evens.ForEach(Console.WriteLine);
+//odds.ForEach(Console.WriteLine);
+
+
+//List<int> filterNumbers(List<int> ints, Func<int, bool> func)
+//{
+//    var lst = new List<int>();
+//    foreach (var item in ints)
+//    {
+//        if(func(item)) lst.Add(item);
+//    }
+//    return lst;
+//}
+//bool isEven(int numb)
+//{
+//    return numb% 2 == 0;
+//}
+
+//bool isOdd(int numb)
+//{
+//    return numb % 2 != 0;
+//}
+
+#endregion
+
+#region Predicate
+// Predicate<T>
+
+//ShowNumbers(numbers, GreaterThanHundred);
+//void ShowNumbers(List<int> ints, Predicate<int> pred)
+//{
+//    foreach (var item in ints)
+//    {
+//        if(pred(item)) Console.WriteLine(item);
+//    }
+//}
+
+//bool GreaterThanHundred(int numb)
+//{
+//    return numb > 100;
+//}
+
+#endregion
+
+#region Comparison
+
+//List<Human> humans = new()
+//{
+//    new Human{Name="Salam", Age = 18},
+//    new Human{Name="Vahid", Age = 15},
+//    new Human{Name="Akif", Age = 35},
+//    new Human{Name="Fuad", Age = 32},
+//    new Human{Name="Hakim", Age = 11},
+//};
+
+////humans.Sort(NameComparison);
+
+//humans.ForEach(Console.WriteLine);
+
+
+//int NameComparison(Human left, Human right)
+//{
+//    return left.Name.CompareTo(right.Name);
+//}
+
+//int AgeComparison(Human left, Human right)
+//{
+//    //return left.Age.CompareTo(right.Age);
+//    if (left.Age > right.Age) return 1;
+//    else if (left.Age < right.Age) return -1;
+//    else return 0;
+//}
+//class Human
+//{
+//    public string? Name { get; set; }
+//    public int Age { get; set; }
+//    public override string ToString()
+//    {
+//        return $"{Name} - {Age}";
+//    }
+//}
 #endregion
