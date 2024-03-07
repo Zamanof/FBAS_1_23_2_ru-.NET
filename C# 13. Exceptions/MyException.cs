e7740e47bcae300df421e6fb4b8c8ecf24649398
Nadir Zamanov <Zamanov@itstep.edu.az>
2024-03-07 18:06:33 +0400

    Extension methods, Exceptions

---
+++ C# 13. Exceptions/MyException.cs
@@ -0,0 +1,21 @@
+﻿/*
+■ исключение должно быть наследником ApplicationException;
+■ у исключения должен быть указан атрибут [Serializable];
+■ у исключения должен быть конструктор по умолчанию;
+■ у исключения должен быть перегруженный конструктор для установки свойства Message;
+■ должен быть перегруженный конструктор для обработки внутренних исключений;
+■ должен быть перегруженный конструктор, выполняющий сериализацию типа
+*/
+
+
+[Serializable]
+public class MyException : Exception
+{
+	public MyException() { }
+	public MyException(string message) : base(message) { }
+	public MyException(string message, Exception inner) : base(message, inner) { }
+	protected MyException(
+	  System.Runtime.Serialization.SerializationInfo info,
+	  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
+
---
+++ C# 13. Exceptions/Program.cs
@@ -0,0 +1,76 @@
+﻿// Exceptions
+int numb1 = 3;
+int numb2 = 0;
+int result = default;
+//result = numb1 / numb2;
+try
+{
+    //result = numb1 / numb2;
+    result = Foo(numb1, numb2);
+    Console.WriteLine(result);
+}
+catch (MyException ex)
+{
+    Console.WriteLine("Catch in Main - MyException");
+    Console.WriteLine(ex.Message);
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Catch in Main - DivideByZeroException");
+    Console.WriteLine();
+    Console.WriteLine($"Message: {ex.Message}");
+    Console.WriteLine();
+    Console.WriteLine($"Target site: {ex.TargetSite}");
+    Console.WriteLine();
+    Console.WriteLine($"Trace: {ex.StackTrace}");
+}
+// exception filters
+catch (Exception ex) when (ex.InnerException != null)
+{
+    Console.WriteLine("Catch in Main - Exception ex with filter");
+    Console.WriteLine();
+    Console.WriteLine($"Message: {ex.Message}");
+    Console.WriteLine();
+    Console.WriteLine($"Target site: {ex.TargetSite}");
+    Console.WriteLine();
+    Console.WriteLine($"Trace: {ex.StackTrace}");
+    Console.WriteLine();
+    Console.WriteLine(ex.InnerException.Message);
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine("Catch in Main - Exception ex without filter");
+    Console.WriteLine();
+    Console.WriteLine($"Message: {ex.Message}");
+    Console.WriteLine();
+    Console.WriteLine($"Target site: {ex.TargetSite}");
+    Console.WriteLine();
+    Console.WriteLine($"Trace: {ex.StackTrace}");
+    Console.WriteLine();
+}
+finally
+{
+    Console.WriteLine("Final block");
+}
+
+int Foo(int a, int b)
+{
+    int result = default;
+    int[] arr = new int[3];
+    try
+    {
+        result = a / b;
+        Console.WriteLine(arr[5]);
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine("Catch in foo");
+        //Console.WriteLine(ex.Message);
+        //throw;
+        //throw new Exception("Salam Aleykum");
+        throw new MyException("This is Spartaaa");
+    }
+
+    return result;
+}
